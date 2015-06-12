b239646280497e52c2b990a8d66132f63f19dda1
Cristian Cisneros <computernerdschris@gmail.com>
2015-06-12 22:49:24 +0000

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,10 @@ Rails.application.routes.draw do
   resources :mma_gyms
 
   get 'pages/mma_gyms'
-
   get 'pages/about'
-
   get 'pages/contact'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
